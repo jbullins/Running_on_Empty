4a9e8040014f930065eb74bb3e4fd040b606d994
jbullins <carolina6strings@yahoo.com>
2022-05-22 15:57:33 -0400

    added alert for character type choice

--- Develop/script.js
+++ Develop/script.js
@@ -8,54 +8,69 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var symbol = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+var choiceChars = "";
 
 function generatePassword() {
   var password = "";
-  var passwordChar = "";
+ 
   // user prompt for password length
-  var passwordLengthUser = prompt("Please choose a password lenght between 8 & 128 characters")
+  var passwordLengthUser = prompt("Please choose a password length between 8 & 128 characters")
   passwordLengthUser = parseInt(passwordLengthUser);
 
   if (passwordLengthUser < 8 || passwordLengthUser > 128) {
     alert("Password must be between 8 and 128 characters")
-    return "";
+    return generatePassword();
   }
-
+  console.log(passwordLengthUser);
   // user prompt for lowercase letters
-  var lowerCaseUser = prompt("Would you like to include lowercase characters?")
-
-  if (lowerCaseUser){
-    passwordChar += lowerCaseUser;
-  }
-
+  var lowerCaseUser = confirm("Would you like to include lowercase characters?")
+  
+  
+ 
+  console.log(lowerCaseUser);
   // user prompt for uppercase letters
-  var upperCaseUser = prompt("Would you like to include uppercase characters?")
+  var upperCaseUser = confirm("Would you like to include uppercase characters?")
 
-  if (upperCaseUser){
-    passwordChar += upperCaseUser;
-  }
-
+  
+  console.log(upperCaseUser);
   // user prompt for numbers
-  var numberCaseUser = prompt("Would you like to include numerical characters?")
+  var numberUser = confirm("Would you like to include numerical characters?")
 
-  if (numberCaseUser){
-    passwordChar += numberCaseUser;
-  }
+  console.log(numberUser);
 
   // user prompt for special characters
-  var symbolCaseUser = prompt("Would you like to include special characters?")
+  var symbolUser = confirm("Would you like to include special characters?")
 
-  if (symbolCaseUser){
-    passwordChar += symbolCaseUser;
+  console.log(symbolUser);
+   
+  if(!lowerCaseUser&&!upperCaseUser&&!numberUser&&!symbolUser){
+    alert("You must choose at least one character type")
+    return generatePassword();
   }
 
+  if(lowerCaseUser){
+    choiceChars += lowerCase
+  }
+  if(upperCaseUser){
+    choiceChars += upperCase
+  }
+  if(numberUser){
+    choiceChars += number
+  }
+  if(symbolUser){
+    choiceChars += symbol
+  }
+
+  for (var i = 0; i < length; i++){
+    password += choiceChars.charAt(Math.floor(Math.random() * choiceChars.length)); 
+  }
+  console.log(password);
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
